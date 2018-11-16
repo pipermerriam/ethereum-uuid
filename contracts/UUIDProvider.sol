c686994d903978be3b3b75895d1cbd24c6766849
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2018-11-16 15:13:47 +0100

    Updated syntax
    Constructor name and visibility
    getByte() visibility specified
    msg.gas => gasleft()
    throw => revert()
    getEntropy() visibility and state mutability change
    block.blockhash => blockhash()
    sha3 => keccak256
    abi.encodePacked for encoding agruments
    collisionCount() visibility specified
    UUID4() visibility specified
    setUUID4Bytes() visibility and state mutability change

--- contracts/UUIDProvider.sol
+++ contracts/UUIDProvider.sol
@@ -1,5 +1,8 @@
+pragma solidity ^0.4.25;
+
+
 contract UUIDProvider {
-    function UUIDProvider() {
+    constructor() public {
         seen[0x0] = true;
         min_entropy = 32;
         addEntropy();
@@ -14,7 +17,7 @@ contract UUIDProvider {
     // entropy when we've been provided enough gas.
     uint constant ADD_ENTROPY_GAS = 750000;
 
-    function getByte() returns (byte b) {
+    function getByte() internal returns (byte b) {
         /*
          *  Entropy sources
          *  - now
@@ -27,7 +30,7 @@ contract UUIDProvider {
          *  - address(this)
          */
         // both return a single byte as well as refilling entropy as needed.
-        if (entropy.length < 2 || (entropy.length < min_entropy && msg.gas > ADD_ENTROPY_GAS)) {
+        if (entropy.length < 2 || (entropy.length < min_entropy && gasleft() > ADD_ENTROPY_GAS)) {
             addEntropy();
             min_entropy += 1;
         }
@@ -37,10 +40,10 @@ contract UUIDProvider {
             entropy.length -= 1;
             return b;
         }
-        throw;
+        revert();
     }
 
-    function addEntropy() public returns (bool) {
+    function addEntropy() internal returns (bool) {
         bytes32 key = getEntropy();
         if (key == 0x0) return false;
         for (uint i = 0; i < 32; i++) {
@@ -50,36 +53,36 @@ contract UUIDProvider {
         return true;
     }
 
-    function getEntropy() constant returns (bytes32 key) {
-        for (uint i = 0; i < 256; i++) {
-            key = block.blockhash(block.number - i);
+    function getEntropy() internal view returns (bytes32 key) {
+        for (uint i = 1; i < 256; i++) {
+            key = blockhash(block.number - i);
             if (!used[key]) return key;
         }
-        key = sha3(now);
+        key = keccak256(abi.encodePacked(now));
         if (!used[key]) return key;
 
-        key = sha3(msg.gas);
+        key = keccak256(abi.encodePacked(gasleft()));
         if (!used[key]) return key;
 
-        key = sha3(block.difficulty);
+        key = keccak256(abi.encodePacked(block.difficulty));
         if (!used[key]) return key;
 
-        key = sha3(tx.origin);
+        key = keccak256(abi.encodePacked(tx.origin));
         if (!used[key]) return key;
 
-        key = sha3(msg.sender);
+        key = keccak256(abi.encodePacked(msg.sender));
         if (!used[key]) return key;
 
-        key = sha3(block.coinbase);
+        key = keccak256(abi.encodePacked(block.coinbase));
         if (!used[key]) return key;
 
-        key = sha3(block.gaslimit);
+        key = keccak256(abi.encodePacked(block.gaslimit));
         if (!used[key]) return key;
 
-        key = sha3(tx.gasprice);
+        key = keccak256(abi.encodePacked(tx.gasprice));
         if (!used[key]) return key;
 
-        key = sha3(address(this));
+        key = keccak256(abi.encodePacked(address(this)));
         if (!used[key]) return key;
 
         return 0x0;
@@ -94,11 +97,11 @@ contract UUIDProvider {
 
     bytes16[] public collisions;
 
-    function collisionCount() constant returns (uint) {
+    function collisionCount() public view returns (uint) {
         return collisions.length;
     }
 
-    function UUID4() returns (bytes16 uuid) {
+    function UUID4() public returns (bytes16 uuid) {
         while (seen[uuid]) {
             if (uuid != 0x0) {
                 collisions.push(uuid);
@@ -115,12 +118,12 @@ contract UUIDProvider {
             }
         }
         seen[uuid] = true;
-        UUID(uuid);
+        emit UUID(uuid);
         prev = uuid;
         return uuid;
     }
 
-    function setUUID4Bytes(bytes16 v) constant returns (bytes16) {
+    function setUUID4Bytes(bytes16 v) internal pure returns (bytes16) {
         byte byte_5 = byte(uint(v) * 2 ** (8 * 5));
         byte byte_7 = byte(uint(v) * 2 ** (8 * 7));
 
